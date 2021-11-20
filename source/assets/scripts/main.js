af54ea8b33df31b00ac41ee6cd44aa4b6027e6fb
klam13630 <66096182+klam13630@users.noreply.github.com>
2021-11-19 19:18:23 -0800

    Update main.js

--- source/assets/scripts/main.js
+++ source/assets/scripts/main.js
@@ -110,7 +110,7 @@ function createRecipeCards(recipes) {
     router.addPage(`${page}-edit`, () => {
       document.querySelector('.section--main-page').classList.remove('shown');
       document.querySelector('.section--recipe-display').classList.remove('shown');
-      document.querySelector('recipe-display').data = recipeData[recipes[i]];
+      document.querySelector('recipe-display').data = recipe;
     });
 
     bindRecipeCard(recipeCard, page);
