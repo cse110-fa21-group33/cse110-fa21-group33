d7043eaef4e1b591ec43ca368b81d8d8cc2d0f57
klam13630 <66096182+klam13630@users.noreply.github.com>
2021-11-23 19:16:34 -0800

    Update main.js

--- source/assets/scripts/main.js
+++ source/assets/scripts/main.js
@@ -100,7 +100,6 @@ function bindRecipeCard(recipeCard, pageName) {
 function bindEditButton(button, pageName) {
   if (button) {
     button.addEventListener('click', (e) => {
-      if (e.path[0].nodeName === 'A') return;
       router.navigate(`${pageName}-edit`);
     });
   }
