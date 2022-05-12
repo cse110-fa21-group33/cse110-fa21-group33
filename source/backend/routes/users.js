ab7c647d59d959993f89f8dffeeaca7f0347ac54
Prem Shelat <prem.shelat@gmail.com>
2022-05-12 11:53:38 -0700

    Added ability to delete a recipe

--- source/backend/routes/users.js
+++ source/backend/routes/users.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const usersModel = require('../database/models/usersModel');
+const savedRecipesModel = require('../database/models/savedReceipesModel');
 
 const router = express.Router();
 
@@ -17,6 +18,33 @@ router.get('/:id', async (req, res) => {
 
     const user = rows[0];
     return res.status(200).json(user);
+  } catch (err) {
+    console.error(err);
+    return res.status(503).json({
+      message: 'Failed to get user information due to'
+        + 'internal server error',
+      err,
+    });
+  }
+});
+
+/* DELETE /user/savedRecipes */
+router.delete('/:savedRecipeId', async (req, res) => {
+  try {
+    const { userId } = req.userInfo;
+    const { recipeId } = req.recipeInfo;
+    if (parseInt(req.params.id, 10) !== userId) {
+      return res.status(401).json({ message: 'Forbidden, acceess denied' });
+    }
+
+    const row = await savedRecipesModel.getByUserIdAndRecipeId(userId, recipeId);
+    if (row.length <= 0) {
+      return res.status(404).json({ message: 'User information not found' });
+    }
+
+    savedRecipesModel.removeById(rowResult[0]);
+    return res.status(200).json("Removed successfully");
+    
   } catch (err) {
     console.error(err);
     return res.status(503).json({
