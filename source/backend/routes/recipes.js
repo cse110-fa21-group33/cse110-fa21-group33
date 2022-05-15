955fe262d437fda86e120ccc9d8d684545903cbf
David Em-Yinn <91344128+demyinn00@users.noreply.github.com>
2022-05-14 17:04:23 -0700

    Update recipes.js

--- source/backend/routes/recipes.js
+++ source/backend/routes/recipes.js
@@ -5,12 +5,8 @@ const router = express.Router();
 
 /* GET /recipes */
 router.get('/:id', async (req, res) => {
-  res.send('chicken');
   try {
     const { recipeId } = req.params.getByRecipeId;
-    //if (parseInt(req.params.id, 10) !== recipeId) {
-    //  return res.status(401).json({ message: 'Forbidden, acceess denied' });
-    //}
     const rows = await recipesModel.getByRecipeId(recipeId);
     if (rows.length === 0) {
       return res.status(404).json({ message: 'Recipe information not found' });
