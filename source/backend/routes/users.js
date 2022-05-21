d644bc1c59e7e43d0bab926c88b0729ea69f1391
Prem Shelat <prem.shelat@gmail.com>
2022-05-21 15:30:11 -0700

    Modifyed removal of saved recipe

--- source/backend/routes/users.js
+++ source/backend/routes/users.js
@@ -42,7 +42,7 @@ router.delete('/:savedRecipeId', async (req, res) => {
       return res.status(404).json({ message: 'User information not found' });
     }
 
-    await savedRecipesModel.removeById(rowResult[0]); 
+    await savedRecipesModel.removeById(row[0].savedRecipeId); 
     return res.status(200).json("Removed successfully");
     
   } catch (err) {
