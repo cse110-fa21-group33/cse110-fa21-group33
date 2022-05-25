1c3e49b4dd10a0b20404b605ab0d26800703262e
shanqingwang <shw019@ucsd.edu>
2022-05-24 21:38:57 -0700

    user signup v1

--- source/backend/database/models/usersModel.js
+++ source/backend/database/models/usersModel.js
@@ -12,4 +12,50 @@ async function getByUserId(userId) {
   return result;
 }
 
-module.exports = { getByUserId };
+/**
+ * get user information by username
+ * @param username
+ * @returns {Promise<awaited Knex.QueryBuilder<TRecord, TResult>>}
+ */
+ async function getByUsername(username) {
+  const result = await db('users')
+    .select('userId', 'email', 'username')
+    .where({ username });
+  return result;
+}
+
+/**
+ * get user information by email
+ * @param email
+ * @returns {Promise<awaited Knex.QueryBuilder<TRecord, TResult>>}
+ */
+ async function getByEmail(email) {
+  const result = await db('users')
+    .select('userId', 'email', 'username')
+    .where({ email });
+  return result;
+}
+
+
+
+/**
+  * create a recipe
+  * @param payload
+  * @returns
+  */
+ async function createUser(payload) {
+  await db.transaction(async (transaction) => {
+    try {
+      const userId = await db('users')
+        .insert(payload)
+        .transacting(transaction)
+        .returning('recipeId');
+      await transaction.commit();
+    } catch (err) {
+      console.log(err);
+      await transaction.rollback();
+    }
+  });
+}
+
+module.exports = { getByUserId, getByUsername, getByEmail, createUser };
--- source/backend/routes/users.js
+++ source/backend/routes/users.js
@@ -3,6 +3,8 @@ const usersModel = require('../database/models/usersModel');
 const completedRecipesModel = require('../database/models/completedRecipesModel');
 const savedRecipesModel = require('../database/models/savedRecipesModel');
 
+const bcrypt = require('bcrypt');
+
 const router = express.Router();
 
 /* GET users/completedRecipes */
@@ -81,6 +83,47 @@ router.delete('/:savedRecipeId', async (req, res) => {
     return res.status(503).json({
       message: 'Failed to get user information due to'
         + 'internal server error',
+      err,
+    });
+  }
+});
+
+/* POST /auth/signup */
+/* Creates new user in database with username, password and email -> unique user/email 
+required */
+router.post('/auth/signup', async (req, res) => {
+  try{
+    const { username } = req.username;
+    const { password } = req.password;
+    const hashedPwd = await bcrypt.hash(password, 10);
+    const { email } = req.email;
+
+    /* check username and email are unique */
+    const checkUser = await usersModel.getByUsername(username);
+    const checkEmail = await usersModel.getByEmail(email);
+
+    if (checkUser.length != 0) {
+      // not sure if status number is correct, 409 = conflict
+      return res.status(409).json({msg: "Username already exists"})
+    }
+    if ( checkEmail.length != 0) {
+      // not sure if status number is correct, 409 = conflict
+      return res.status(409).json({msg: "E-mail already in use"})
+    }
+
+    /* TODO: create new user */ 
+    const newUser = req.body.user;
+    newUser.password = hashedPwd;
+
+    /* TODO: insert user into database */
+    await usersModel.createUser(newUser);
+
+    return res.status(200).json({ newUser, msg: 'Successfully created a new user' });
+
+  } catch (err) {
+    console.error(err);
+    return res.status(503).json({
+      message: 'Failed to register new user',
       err,
     });
   }
