304a1c44353e12b9e831cbfd47f0883318c77e46
Thomas Li <thomasligreat11@gmail.com>
2022-05-25 00:09:33 -0700

    fixed bcrypt

--- source/backend/routes/auth.js
+++ source/backend/routes/auth.js
@@ -1,28 +1,12 @@
 const express = require('express');
-const bcrypt = require('bcrypt');
-const jwt = require('jsonwebtoken');
-const db = require('../database/dbConfig');
+const bcrypt = require('bcrypt-nodejs');
+const tokenUtil = require('../auth/tokenUtil');
+const usersModel = require('../database/models/usersModel');
+
 const router = express.Router();
 
-/**
- * get user information by username
- * @param username
- * @param userId
- * @returns {Promise<awaited Knex.QueryBuilder<TRecord, TResult>>}
- */
- async function getUserByUsername(username) {
-  const result = await db('users')
-    .select('username', 'userId', 'password')
-    .where({ username });
-  return result;
-}
-
-
 /* GET /auth */
-router.get('/', (req, res) => {
-  return res.status(200).json({msg: 'passed'});
-});
-
+router.get('/', (req, res) => res.status(200).json({ msg: 'passed' }));
 
 /*
   Find username and password (hashed by bcrypt or some other hash functions)
@@ -33,29 +17,47 @@ router.get('/', (req, res) => {
        Return 404 (not found)
 */
 router.post('/login', async (req, res) => {
-  const { username, password } = req.body;
+  try {
+    const {
+      username,
+      password,
+    } = req.body;
 
-  // ensure user info is entered 
-  if (!username || !password) return res.status(400).json({ 'message': 'Username and password are requried.' });
-  
-  // find user
-  const rows = await getUserByUsername(username);
-  const foundPassword = rows[0]['password'];
-  
-  // evaluate passwords
-  const match = await bcrypt.compare(password, foundPassword);
+    // ensure user info is entered
+    if (!username || !password) {
+      return res.status(400)
+        .json({ message: 'Username and password are requried.' });
+    }
 
-  // determine if user exists in database
-  if (rows.length === 0 || !match) {
-    return res.status(404).json({ message: 'User information not found' });
+    // find user
+    const rows = await usersModel.getByUsername(username);
+    const foundPassword = rows[0].password;
+    // evaluate passwords
+    const isPasswordMatched = bcrypt.compareSync(password, foundPassword);
+
+    // determine if user exists in database
+    if (rows.length === 0 || !isPasswordMatched) {
+      return res.status(404)
+        .json({ message: 'User information not found' });
+    }
+
+    // get userId and password
+    const user = {
+      userId: rows[0].userId,
+      password: rows[0].password,
+    };
+
+    // return JWT token
+    const accessToken = await tokenUtil.generateToken(user);
+    return res.status(200)
+      .json({ accessToken });
+  } catch (err) {
+    console.error(err);
+    return res.status(503).json({
+      message: 'Failed to login due to internal error',
+      err,
+    });
   }
-  
-  // get userId and password
-  const user = { userId: rows[0]['userId'], password: rows[0]['password'] };
-
-  // return JWT token
-  const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET);
-  return res.status(200).json({ accessToken: accessToken });
 });
 
 module.exports = router;
