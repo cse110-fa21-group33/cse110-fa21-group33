1fcd87af33b279d1d1839c5724584fef20744a54
Solowyhn <47937357+Solowyhn@users.noreply.github.com>
2022-05-26 15:42:42 -0700

    finish testing, should be good

--- source/backend/routes/recipes.js
+++ source/backend/routes/recipes.js
@@ -39,13 +39,12 @@ router.put('/:recipeId', verifyUserToken, async (req, res) => {
       return res.status(401).json({msg: 'Unauthorized to edit recipe'});
     }
 
+    const update = req.body;
     const updatedRecipe = req.body.recipe;
-    console.log(updatedRecipe);
-    
     const updatedIngredients = req.body.ingredients;
     await recipesModel.updateRecipe(updatedRecipe, updatedIngredients, recipeId);
 
-    return res.status(200).json({updatedRecipe, msg: 'Successfully edited a recipe'});
+    return res.status(200).json({update, msg: 'Successfully edited a recipe'});
   } catch (err) {
     console.error(err);
     return res.status(500).json({
